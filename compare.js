5b6d27eb2c37281d2adefce8475e82780255478a
Saksham08 S <saksham080106@gmail.com>
2024-10-05 22:49:42 +0530

    file updated

---
+++ compare.js
@@ -0,0 +1,5 @@
+console.log(2>1);
+console.log("2">1);  // 2 is converted into integer and returns true
+console.log(null >=0); // null is converted into zero returns true
+console.log("2"==2);
+console.log("2"===2); // === checks datatype also string!=integer
